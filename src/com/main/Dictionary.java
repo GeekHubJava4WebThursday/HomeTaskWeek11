c6e40003b90b56361caebb629513d3d6f8502c77
Sergiy Govorukhin <govorukhin@gmail.com>
2016-01-25 02:36:15 +0200

    Updated translate method

--- src/com/main/Dictionary.java
+++ src/com/main/Dictionary.java
@@ -6,10 +6,9 @@ import org.springframework.stereotype.Component;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
+import java.nio.charset.Charset;
+import java.util.*;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 @Component
@@ -17,18 +16,34 @@ public class Dictionary {
 
     private Map<Language, Map<String, String>> dictionaries = new HashMap<>();
 
-    public static final String delimiterPattern = "[^a-zA-Z]";
+    public static final Pattern wordPattern = Pattern.compile("[^a-zA-Z]+", Pattern.DOTALL);
 
     public String translate(String text, Language language) {
         Map<String, String> dictionary = getDictionary(language);
-        return Arrays
-                .stream(text.split(delimiterPattern))
-                .map(word -> {
-                    String value = dictionary.get(word.toLowerCase());
-                    return (value == null) ? word : value;
-                })
-                .collect(Collectors.joining(" "));
+        Set<String> words = Arrays
+                .stream(wordPattern.split(text))
+                .collect(Collectors.toSet());
+        for (String word: words) {
+            String translation = dictionary.get(word.toLowerCase());
+            if (translation != null) {
+                text = text.replaceAll("\\b" + word + "\\b", toCapital(translation, word));
+            }
+        }
+        return text;
 	}
+
+    private String toCapital(String translation, String origin) {
+        if (origin.length() < 1 || translation.length() < 1) {
+            return origin;
+        }
+        if (translation.length() < 1 || !Character.isUpperCase(origin.charAt(0))) {
+            return translation;
+        }
+        if (translation.length() == 1) {
+            return translation.toUpperCase();
+        }
+        return translation.substring(0, 1).toUpperCase() + translation.substring(1);
+    }
 
 	private Map<String, String> getDictionary(Language language) {
 		Map<String, String> dictionary = dictionaries.get(language);
@@ -43,7 +58,8 @@ public class Dictionary {
 //        List<String> dictionary = resourceLoader.load(System.getProperty("user.dir") + "\\dict\\english.dict");
         Properties properties = new Properties();
         String propertyFile = System.getProperty("user.dir") + "\\dict\\" + language.toString().toLowerCase() + ".dict";
-        try (InputStreamReader inputStream = new InputStreamReader(new FileInputStream(propertyFile))) {
+        try (InputStreamReader inputStream = new InputStreamReader(
+                new FileInputStream(propertyFile), Charset.defaultCharset())) {
             properties.load(inputStream);
         } catch (IOException e) {
             e.printStackTrace();
