af610dee762671d1ab55bfd20ca82c61d3482f56
Oleksyuk Vadim <R.Oleksuk@yandex.ru>
2016-01-31 23:05:35 +0200

    Home work 11 spring init

--- src/com/main/AppConfiguration.java
+++ src/com/main/AppConfiguration.java
@@ -1,11 +1,15 @@
 package com.main;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
+@ComponentScan("com")
 public class AppConfiguration {
 
-    //TODO: Implement me
+    Translator translator() {
+        return new Translator();
+    }
 }
--- src/com/main/Translator.java
+++ src/com/main/Translator.java
@@ -1,14 +1,38 @@
 package com.main;
 
+import com.lang.Language;
+import com.lang.LanguageDetector;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.util.StringJoiner;
 
+@Component
 public class Translator {
 
+    private static String WORD_DELIMITER = " ";
+
+    @Autowired
+    private TextSource textSource;
+
+    @Autowired
+    private Dictionary dictionary;
+
+    @Autowired
+    private LanguageDetector languageDetector;
+
     public String translate(String source) {
-        //TODO: Implement me
-		return null;
+        String text = textSource.getText(source);
+        Language fromLanguage = languageDetector.detectLanguage(text);
+        String[] words = text.split(WORD_DELIMITER);
+        StringJoiner translatedText = new StringJoiner(WORD_DELIMITER);
+        for (String word:words){
+            String punctuation = word.replaceAll("[^\\W]|_", "");
+            String translatedWord = dictionary.translate(word.replaceAll("[\\W]|_", ""),fromLanguage);
+            translatedText.add(translatedWord+punctuation);
+        }
+        return translatedText.toString();
 	}
 
 }
